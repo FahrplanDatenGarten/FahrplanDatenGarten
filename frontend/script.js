0cfc95ad675e81f460256fe385c3e426c9f5c96d
wusel905@gmail.com <wusel905@gmail.com>
2019-10-05 15:18:19 +0200

    fix load bug

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
     load();
 });
-function load(){
+function load() {
     $("#body").hide();
     $("#loading").show();
     $.ajax({
         url: $("#content").attr("view"),
         dataType: 'html'
-    }).done(function(data){
+    }).done(function (data) {
         $("#content").html(data);
-    });
-    var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
-    $.ajax({
-        url: url,
-        dataType: 'json'
-    })
-    .done(function(data){
-        console.log(data);
-        var currentdelayed = data.journeys_delayed;
-        var mostNumber = data.biggest_delay[0].name;
-        var mostMinutes = data.biggest_delay[0].delay;
-        var currentaverage = data.average_delay;
-        //console.log(bio);
-        $("#train-count").text(currentdelayed);
-        $("#most-number").text(mostNumber);
-        $("#most-minutes").text(mostMinutes);
-        $("#average").text(currentaverage);
+        var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
+        $.ajax({
+            url: url,
+            dataType: 'json'
+        })
+            .done(function (data) {
+                console.log(data);
+                var currentdelayed = data.journeys_delayed;
+                var mostNumber = data.biggest_delay[0].name;
+                var mostMinutes = data.biggest_delay[0].delay;
+                var currentaverage = data.average_delay;
+                //console.log(bio);
+                $("#train-count").text(currentdelayed);
+                $("#most-number").text(mostNumber);
+                $("#most-minutes").text(mostMinutes);
+                $("#average").text(currentaverage);
 
-        $("#loading").fadeOut(function(){
-            $("#body").fadeIn();
-            
-            
-            $(".ajax-link").each(function(){
-                $(this).click(function(event){
-                    $("#content").attr("view",$(this).attr("href"));
-                    load();
-                    event.preventDefault();
-                    return false;
+                $("#loading").fadeOut(function () {
+                    $("#body").fadeIn();
+
+
+                    $(".ajax-link").each(function () {
+                        $(this).click(function (event) {
+                            $("#content").attr("view", $(this).attr("href"));
+                            load();
+                            event.preventDefault();
+                            return false;
+                        });
+                    });
                 });
             });
-        });
     });
+
 }
