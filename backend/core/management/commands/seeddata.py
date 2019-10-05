283ed13ee884298fc1fe652c8fc996610c834106
root <root@localhost.localdomain>
2019-10-05 13:28:38 -0400

    views almost done

--- backend/core/management/commands/seeddata.py
+++ backend/core/management/commands/seeddata.py
@@ -9,21 +9,30 @@ class Command(BaseCommand):
     help = "Seeds (fake) data in database"
 
     def handle(self, *args, **options):
-        agency = Agency.objects.update_or_create(name="DB")
-        source = Source.objects.update_or_create(name="DBApis")
-        stop = Stop.objects.update_or_create(ifopt="de:05315:11201")
+        agency,_ = Agency.objects.update_or_create(name="DB")
+        source,_ = Source.objects.update_or_create(name="DBApis")
+        stop,_ = Stop.objects.update_or_create(ifopt="de:05315:11201")
         StopName.objects.update_or_create(stop=stop,
                                 name="Köln Hbf",
                                 source=source,
                                 priority=1)
         StopID.objects.update_or_create(stop=stop, source_stop_id="8000207",
                               source=source, source_stop_id_type="EVA")
-        journey = Journey.objects.create(name="ICE 557", date=datetime.datetime.now(),
+        journey = Journey.objects.create(name="ICE 558", date=datetime.datetime.now(),
                                          journey_id="123jd213/12312hd", source=source,
                                          agency=agency)
         JourneyStop.objects.create(stop=stop,
                                    journey=journey,
-                                   planned_arrival_time=datetime.datetime(2019, 9, 12, 12, 00, tzinfo=pytz.utc),
-                                   actual_arrival_time=datetime.datetime(2019, 9, 12, 17, 00, tzinfo=pytz.utc),
-                                   planned_departure_time=datetime.datetime(2019, 9, 12, 13, 00, tzinfo=pytz.utc),
-                                   actual_departure_time=datetime.datetime(2019, 9, 12, 20, 00, tzinfo=pytz.utc))
+                                   planned_arrival_time=datetime.datetime(2019, 10, 4, 19, 11, tzinfo=pytz.utc),
+                                   actual_arrival_time=datetime.datetime(2019, 10, 4, 23, 59, tzinfo=pytz.utc),
+                                   planned_departure_time=datetime.datetime(2019, 10, 4, 9, 00, tzinfo=pytz.utc),
+                                   actual_departure_time=datetime.datetime(2019, 10, 4, 19, 11, tzinfo=pytz.utc))
+
+        JourneyStop.objects.create(stop=stop,
+                                   journey=journey,
+                                   planned_arrival_time=datetime.datetime(2019, 10, 6, 19, 11, tzinfo=pytz.utc),
+                                   actual_arrival_time=datetime.datetime(2019, 10, 6, 23, 59, tzinfo=pytz.utc),
+                                   planned_departure_time=datetime.datetime(2019, 10, 6, 9, 00, tzinfo=pytz.utc),
+                                   actual_departure_time=datetime.datetime(2019, 10, 6, 19, 11, tzinfo=pytz.utc))
+
+
