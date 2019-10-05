615816790fcced16328968ea5a6b1b7b41153dda
Anna <maedchenkunst@gmail.com>
2019-10-05 15:19:58 +0200

    update to js

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
-    $("#body").hide();
-    $("#loading").show();
-    var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
-    $.ajax({
-        url: url,
-        dataType: 'json'
-    })
-    .done(function(data){
-        console.log(data);
-        var currentdelayed = data.journeys_delayed;
-        var mostNumber = data.biggest_delay[0].name;
-        var mostMinutes = data.biggest_delay[0].delay;
-        var currentaverage = data.average_delay;
-        //console.log(bio);
-        $("#train-count").text(currentdelayed);
-        $("#most-number").text(mostNumber);
-        $("#most-minutes").text(mostMinutes);
-        $("#average").text(currentaverage);
+$(document).ready(function () {
+	$("#body").hide();
+	$("#loading").show();
+	var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
+	$.ajax({
+			url: url,
+			dataType: 'json'
+		})
+		.done(function (data) {
+			console.log(data);
+			var currentdelayed = data.journeys_delayed;
+			var mostNumber = data.biggest_delay[0].name;
+			var mostMinutes = data.biggest_delay[0].delay;
+			var currentaverage = data.average_delay;
+			var averagejourneys = data.journeys_delayed / data.current_journeys;
+			averagejourneys = Math.round(averagejourneys * 100);
 
-        $("#loading").fadeOut(function(){
-            $("#body").fadeIn();
-        });
-    });
-    
+			$("#train-count").text(averagejourneys + "%");
+			$("#most-number").text(mostNumber);
+			$("#most-minutes").text(mostMinutes);
+			$("#average").text(currentaverage);
+			$("#loading").fadeOut(function () {
+				$("#body").fadeIn();
+			});
+
+			//console.log(averagejourneys);
+		});
+
 });
