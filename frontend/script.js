a35754a6ff665ce833c983514f44b605ca3ffae3
wusel905@gmail.com <wusel905@gmail.com>
2019-10-05 15:24:53 +0200

    add procent to ajax

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,46 @@
 $(document).ready(function () {
-	$("#body").hide();
-	$("#loading").show();
-	var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
-	$.ajax({
-			url: url,
-			dataType: 'json'
-		})
-		.done(function (data) {
-			console.log(data);
-			var currentdelayed = data.journeys_delayed;
-			var mostNumber = data.biggest_delay[0].name;
-			var mostMinutes = data.biggest_delay[0].delay;
-			var currentaverage = data.average_delay;
-			var averagejourneys = data.journeys_delayed / data.current_journeys;
-			averagejourneys = Math.round(averagejourneys * 100);
-
-			$("#train-count").text(averagejourneys + "%");
-			$("#most-number").text(mostNumber);
-			$("#most-minutes").text(mostMinutes);
-			$("#average").text(currentaverage);
-			$("#loading").fadeOut(function () {
-				$("#body").fadeIn();
-			});
-
-			//console.log(averagejourneys);
-		});
-
+    load();
 });
+function load() {
+    $("#body").hide();
+    $("#loading").show();
+    $.ajax({
+        url: $("#content").attr("view"),
+        dataType: 'html'
+    }).done(function (data) {
+        $("#content").html(data);
+        var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
+        $.ajax({
+            url: url,
+            dataType: 'json'
+        })
+            .done(function (data) {
+                console.log(data);
+                var currentdelayed = data.journeys_delayed;
+                var mostNumber = data.biggest_delay[0].name;
+                var mostMinutes = data.biggest_delay[0].delay;
+                var currentaverage = data.average_delay;
+                //console.log(bio);
+                
+			    $("#train-count").text(averagejourneys + "%");
+                $("#most-number").text(mostNumber);
+                $("#most-minutes").text(mostMinutes);
+                $("#average").text(currentaverage);
+
+                $("#loading").fadeOut(function () {
+                    $("#body").fadeIn();
+
+
+                    $(".ajax-link").each(function () {
+                        $(this).click(function (event) {
+                            $("#content").attr("view", $(this).attr("href"));
+                            load();
+                            event.preventDefault();
+                            return false;
+                        });
+                    });
+                });
+            });
+    });
+
+}
