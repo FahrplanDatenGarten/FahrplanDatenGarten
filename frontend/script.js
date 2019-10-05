971a85472f354d43752c3cc7aaa69ecf9f6b2377
wusel905@gmail.com <wusel905@gmail.com>
2019-10-05 17:32:02 +0200

    add chart

--- frontend/script.js
+++ frontend/script.js
@@ -21,10 +21,41 @@ function load() {
                 var mostMinutes = data.biggest_delay[0].delay;
                 var currentaverage = data.average_delay;
                 var averagejourneys = data.journeys_delayed / data.current_journeys;
-			    averagejourneys = (averagejourneys*100).toFixed(2);
-			
+                averagejourneys = (averagejourneys*100).toFixed(2);
                 
-			    $("#train-count").text(averagejourneys + "%");
+
+
+                var ctx = document.getElementById('train-chart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+                
+			    $("#train-chart").text(averagejourneys + "%");
                 $("#most-number").text(mostNumber);
                 $("#most-minutes").text(mostMinutes);
                 $("#average").text(currentaverage);
