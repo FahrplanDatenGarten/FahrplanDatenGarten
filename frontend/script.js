d6162661f4fabc85691a67511e8bd0d5e8ff9c2c
Anna <maedchenkunst@gmail.com>
2019-10-05 17:47:32 +0200

    js update

--- frontend/script.js
+++ frontend/script.js
@@ -1,64 +1,75 @@
 $(document).ready(function () {
-	
+	load();
 });
 
 function load() {
-    $("#body").hide();
-    $("#loading").show();
-    $.ajax({
-        url: $("#content").attr("view"),
-        dataType: 'html'
-    }).done(function (data) {
-        $("#content").html(data);
-        var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
-        $.ajax({
-            url: url,
-            dataType: 'json'
-        })
-            .done(function (data) {
-                console.log(data);
-                var averagejourneys = data.average_journeys;
-                var mostNumber = data.biggest_delay[0].name;
-                var mostMinutes = data.biggest_delay[0].delay;
-                var currentaverage = data.average_delay;
-                var averagejourneys = data.journeys_delayed / data.current_journeys;
-			    averagejourneys = (averagejourneys*100).toFixed(2);
-			
-                
-			    $("#train-count").text(averagejourneys + "%");
-                $("#most-number").text(mostNumber);
-                $("#most-minutes").text(mostMinutes);
-                $("#average").text(currentaverage);
+	$("#body").hide();
+	$("#loading").show();
+	$.ajax({
+		url: $("#content").attr("view"),
+		dataType: 'html'
+	}).done(function (data) {
+		$("#content").html(data);
+		standartdaten();
+		pageload();
+		formular();
+	});
 
-
-
-                console.log("123");
-                var zugformular = $("#zugformular");
-                if (zugformular.length){
-                    console.log("test");
-                    zugformular.closest('form').on('submit', function(e){
-                
-                        console.log("Hello");
-                        e.preventDefault();
-                        
-                    });
-                }
-                $("#loading").fadeOut(function () {
-                    $("#body").fadeIn();
-
-
-                    $(".ajax-link").each(function () {
-                        $(this).click(function (event) {
-                            $("#content").attr("view", $(this).attr("href"));
-                            load();
-                            event.preventDefault();
-                            return false;
-                        });
-                    });
-                });
-            });
-    });
-	
 
 }
 
+function pageload() {
+	$("#loading").fadeOut(function () {
+		$("#body").fadeIn();
+
+
+		$(".ajax-link").each(function () {
+			$(this).click(function (event) {
+				$("#content").attr("view", $(this).attr("href"));
+				load();
+				event.preventDefault();
+				return false;
+			});
+		});
+	});
+
+}
+
+function formular() {
+	console.log("123");
+	var zugformular = $("#zugformular");
+	if (zugformular.length) {
+		console.log("test");
+		zugformular.closest('form').on('submit', function (e) {
+
+			console.log("Hello");
+			e.preventDefault();
+
+		});
+	}
+
+}
+
+function standartdaten() {
+	var url = "https://raw.githubusercontent.com/Jugendhackt/FahrplanDatenGarten/master/demo.json";
+	$.ajax({
+			url: url,
+			dataType: 'json'
+		})
+		.done(function (data) {
+			console.log(data);
+			//standarddaten
+			var averagejourneys = data.average_journeys;
+			var mostNumber = data.biggest_delay[0].name;
+			var mostMinutes = data.biggest_delay[0].delay;
+			var currentaverage = data.average_delay;
+			var averagejourneys = data.journeys_delayed / data.current_journeys;
+			averagejourneys = (averagejourneys * 100).toFixed(2);
+
+
+			$("#train-count").text(averagejourneys + "%");
+			$("#most-number").text(mostNumber);
+			$("#most-minutes").text(mostMinutes);
+			$("#average").text(currentaverage);
+		});
+}
