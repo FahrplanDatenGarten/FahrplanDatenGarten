79b8a64adb58c932f720c416f7a8c28170a28e72
Simeon Keske <ca-git@noemis.me>
2019-10-06 11:01:08 +0200

    fix current journeys

--- verspaeti/views.py
+++ verspaeti/views.py
@@ -16,30 +16,34 @@ def get_demojson(request):
 
 
 def convert_toJson(request):
-	current_journeys = models.Journey.objects.filter(
-		stop__journeystop__actual_departure_time__lte = datetime.datetime.now()).filter(
-		stop__journeystop__actual_arrival_time__gte = datetime.datetime.now()
-	).distinct()
-	current_stops = models.JourneyStop.objects.filter(journey__in = current_journeys,actual_departure_time__lte = datetime.datetime.now()).order_by('actual_departure_time')
-	delayed_stops = {}
-	for stop in current_stops:
-		delayed_stops[stop.journey]={
-		        "name": stop.journey.name,
-      			"plannedDepature": stop.planned_departure_time,
-       		        "actualDepature": stop.actual_departure_time,
-    			"delay": (stop.actual_departure_time-stop.planned_departure_time).total_seconds() / 60,
-    			"date": stop.journey.date,
-     			"id": stop.journey.journey_id,
-     			"source": stop.journey.source.name,
-     			"agency": stop.journey.agency.name
-		}
-	delayed_stops = list(x for x in sorted(delayed_stops.values(),key = lambda d : d['delay'],reverse=True) if x['delay'] >= 5)
-	returnDict = {
-		"current_journeys":  current_journeys.count(),
-		"journeys_delayed": len({d['name'] for d in delayed_stops}),
-		"biggest_delay": delayed_stops,
-		"average_delay": (sum([d['delay'] for d in delayed_stops]) / len(delayed_stops)) if delayed_stops else None
-	}
+    current_journeys = models.Journey.objects.filter(
+        stop__journeystop__actual_departure_time__lte=datetime.datetime.now()).filter(
+        stop__journeystop__actual_arrival_time__gte=datetime.datetime.now()
+    ).distinct()
+    current_stops = models.JourneyStop.objects.filter(
+        journey__in=current_journeys, actual_departure_time__lte=datetime.datetime.now()).order_by('actual_departure_time')
+    delayed_stops = {}
+    for stop in current_stops:
+        delayed_stops[stop.journey] = {
+                "name": stop.journey.name,
+        			"plannedDepature": stop.planned_departure_time,
+        		        "actualDepature": stop.actual_departure_time,
+        		"delay": (stop.actual_departure_time - stop.planned_departure_time).total_seconds() / 60,
+        		"date": stop.journey.date,
+        			"id": stop.journey.journey_id,
+        			"source": stop.journey.source.name,
+        			"agency": stop.journey.agency.name
+        }
+    delayed_stops = list(x for x in sorted(delayed_stops.values(
+    ), key=lambda d: d['delay'], reverse=True) if x['delay'] >= 5)
+    returnDict = {
+    	"current_journeys":  current_journeys.count(),
+    	"journeys_delayed": len({d['name'] for d in delayed_stops}),
+    	"biggest_delay": delayed_stops,
+    	"average_delay": (sum([d['delay'] for d in delayed_stops]) / len(delayed_stops)) if delayed_stops else None
+    }
+
+    return JsonResponse(returnDict,encoder=DjangoJSONEncoder)
 
 
 class IndexView(TemplateView):
