45bc9fb9e4b89ef86785c18b11d732aadf2102cd
Leo Maroni <me@leomaroni.de>
2020-03-23 11:44:05 +0100

    Cache verspaeti for 30min

--- FahrplanDatenGarten/verspaeti/urls.py
+++ FahrplanDatenGarten/verspaeti/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'verspaeti'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', cache_page(60 * 30)(views.IndexView.as_view()), name='index'),
 ]
