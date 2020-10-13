79bde3d6ce0f5610e8e79b81ffb2f8304a8da015
Leo Maroni <git@em0lar.de>
2020-10-13 21:02:24 +0200

    Use matplotlib instead of plotly

--- FahrplanDatenGarten/verspaeti/views.py
+++ FahrplanDatenGarten/verspaeti/views.py
@@ -1,7 +1,9 @@
+import base64
+from io import BytesIO
+
 from django.core.cache import cache
 from django.views.generic import TemplateView
-from plotly.graph_objs import Pie
-from plotly.offline import plot
+from matplotlib import pyplot
 
 
 class IndexView(TemplateView):
@@ -11,20 +13,33 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         verspaeti_data_cache = cache.get('verspaeti_data')
         if verspaeti_data_cache is not None:
-            colors = ['#63a615', '#ec0016']
-            labels = ['Pünktlich', 'Zu spät']
+            colors = ('#63a615', '#ec0016')
+            labels = ('Pünktlich', 'Zu spät')
             values = [
                 verspaeti_data_cache['num_current_journeys'] -
                 verspaeti_data_cache['num_delayed_journeys'],
                 verspaeti_data_cache['num_delayed_journeys']]
-            plot_div = plot(
-                [Pie(
-                    labels=labels,
-                    values=values,
-                    marker=dict(colors=colors))],
-                output_type='div')
+            plot_figure, plot_axes = pyplot.subplots(figsize=(5, 6))
 
-            context['plot_div'] = plot_div
+            plot_wedges, _, plot_autotexts = pyplot.pie(
+                values,
+                colors=colors,
+                autopct='%1.1f%%')
+
+            plot_axes.legend(
+                plot_wedges,
+                labels,
+                loc="lower center",
+                fontsize="xx-large",
+                bbox_to_anchor=(0.5, -0.1)
+            )
+            pyplot.setp(plot_autotexts, size=20)
+            pyplot.axis('equal')
+            plot_temporary_file = BytesIO()
+            pyplot.savefig(plot_temporary_file, format='png')
+            context['plot_image_base64'] = base64.b64encode(
+                plot_temporary_file.getvalue()).decode('utf-8')
+
             context['journeys_delayed'] = verspaeti_data_cache['num_delayed_journeys'],
             context['biggest_delay_name'] = verspaeti_data_cache['biggest_delay_name']
             context['biggest_delay_time'] = verspaeti_data_cache['biggest_delay_time']
