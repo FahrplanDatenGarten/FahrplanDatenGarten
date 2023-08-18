2d5deeab2893a74cd7e9082231d1adc211678cc5
Leona Maroni <dev@leona.is>
2023-08-18 23:08:03 +0200

    allow customizing config file path, publish nix flake overlay

--- flake.nix
+++ flake.nix
@@ -2,45 +2,61 @@
     description = "";
     inputs.nixpkgs.url = "github:NixOS/nixpkgs";
     inputs.flake-utils.url = "github:numtide/flake-utils";
-    outputs = {self, nixpkgs, flake-utils}:
-      flake-utils.lib.eachDefaultSystem(system:
-        let
-          overrides = pkgs: {
-            fdfgen = pkgs.fdfgen.overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
-            });
-            pyhafas = pkgs.pyhafas.overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
-            });
-            django-bootstrap4 = pkgs.django-bootstrap4.overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or []) ++ [pkgs.hatchling];
-            });
-            contourpy = pkgs.contourpy.override {
-              preferWheel = true;
-            };
+    outputs = {self, nixpkgs, flake-utils}: let
+      # this is ugly, we should move the overrides to a more top position
+      overrides = pkgs: {
+        fdfgen = pkgs.fdfgen.overridePythonAttrs (old: {
+            buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
+        });
+        pyhafas = pkgs.pyhafas.overridePythonAttrs (old: {
+            buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
+        });
+        django-bootstrap4 = pkgs.django-bootstrap4.overridePythonAttrs (old: {
+            buildInputs = (old.buildInputs or []) ++ [pkgs.hatchling];
+        });
+        contourpy = pkgs.contourpy.override {
+          preferWheel = true;
+        };
+      };
+    in {
+      overlay = final: prev: {
+        fahrplandatengarten-app = prev.poetry2nix.mkPoetryApplication {
+          projectDir = self;
+          overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: (overrides super));
+        };
+        fahrplandatengarten = final.fahrplandatengarten-app.dependencyEnv;
+      };
+    } // flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+        overrides = pkgs: {
+          fdfgen = pkgs.fdfgen.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
+          });
+          pyhafas = pkgs.pyhafas.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or []) ++ [pkgs.setuptools];
+          });
+          django-bootstrap4 = pkgs.django-bootstrap4.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or []) ++ [pkgs.hatchling];
+          });
+          contourpy = pkgs.contourpy.override {
+            preferWheel = true;
           };
-          overlay = final: prev: {
-            fahrplandatengarten-app = prev.poetry2nix.mkPoetryApplication {
-              projectDir = ./.;
-              overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: (overrides super));
-            };
-            fahrplandatengarten = final.fahrplandatengarten-app.dependencyEnv;
+        };
+      in {
+        defaultPackage = pkgs.fahrplandatengarten;
+        packages = { inherit (pkgs) fahrplandatengarten; };
+        devShell = (pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = self;
+          editablePackageSources = {
+            fahrplandatengarten = ./fahrplandatengarten;
           };
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          };
-        in {
-          defaultPackage = pkgs.fahrplandatengarten;
-          packages = { inherit (pkgs) fahrplandatengarten; };
-          devShells.default = (pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-            editablePackageSources = {
-              fahrplandatengarten = ./fahrplandatengarten;
-            };
-            overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super: (overrides super));
-          }).env;
-        }
-      );
+          overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super: (overrides super));
+        }).env;
+      }
+    );
 }
 
