7ab43956590d3a828c4aa093f2a1f3c801e1c64b
Leona Maroni <dev@leona.is>
2024-06-15 14:19:43 +0200

    fix: skip opendata_stop csv import

--- fahrplandatengarten/DBApis/csvImport.py
+++ fahrplandatengarten/DBApis/csvImport.py
@@ -5,6 +5,8 @@ import requests
 
 
 def parse_db_opendata_stop_csv():
+    # The csv is unavailable. as we already have the required data, we can skip the import
+    return []
     r = requests.get(
         'http://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV')
     r.encoding = 'utf-8'
